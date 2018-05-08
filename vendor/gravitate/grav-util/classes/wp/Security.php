30bca62a35be0118094d141d0bb022f94718b929
Doug Frei <doug.frei@gravitatedesign.com>
2018-05-08 14:41:42 -0700

    updated grav-util composer package

--- vendor/gravitate/grav-util/classes/wp/Security.php
+++ vendor/gravitate/grav-util/classes/wp/Security.php
@@ -2,12 +2,12 @@
 namespace Grav\WP;
 
 class Security {
-	public static function disable_dashboard_access_for_roles($roles) {
-		add_action('init', function() use (&$roles) {
+	public static function disable_dashboard_access_for_roles($roles, $redirect_url = '') {
+		add_action('init', function() use (&$roles, &$redirect_url) {
 			if (is_admin() && !defined('DOING_AJAX')) {
 				foreach ($roles as $role) {
 					if (current_user_can($role)) {
-						wp_redirect(home_url());
+						wp_redirect($redirect_url ? $redirect_url : home_url());
 						exit;
 					}
 				}
@@ -15,13 +15,13 @@ class Security {
 		});
 	}
 
-	public static function set_mimimum_dashboard_access_level($min_level=1) {
-		add_action('init', function() use (&$roles) {
+	public static function set_mimimum_dashboard_access_level($min_level=1, $redirect_url = '') {
+		add_action('init', function() use (&$roles, &$redirect_url) {
 			if (is_admin() && !defined('DOING_AJAX')) {
 				$cur_user = wp_get_current_user();
 
 				if (!$cur_user || !isset($cur_user->allcaps['level_'.$minimum_level]) || !$cur_user->allcaps['level_'.$minimum_level]) {
-					wp_redirect(home_url());
+					wp_redirect($redirect_url ? $redirect_url : home_url());
 					exit;
 				}
 			}
