06b7952991c9e5977dfec6d32eb48168e16fe476
Doug Frei <dougfrei@gmail.com>
2019-01-31 10:17:14 -0800

    prevent admin errors when ACF is not enabled

--- bootstrap/acf.php
+++ bootstrap/acf.php
@@ -3,6 +3,8 @@
 define('ACF_LITE', defined('ENVIRONMENT') ? ENVIRONMENT !== 'local' : true);
 
 // include all ACF files from the 'acf' path (exclude files that begin with '_")
-Grav\WP\Util::include_all_files(get_template_directory().'/acf/*.php', function($file) {
-	return substr(basename($file), 0, 1) !== '_';
-});
+if (function_exists('acf_add_local_field_group')) {
+	Grav\WP\Util::include_all_files(get_template_directory().'/acf/*.php', function($file) {
+		return substr(basename($file), 0, 1) !== '_';
+	});
+}
