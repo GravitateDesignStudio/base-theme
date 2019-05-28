9fd952f6f06e10867171e2fabac3dc1782f9990d
Doug Frei <dougfrei@gmail.com>
2019-05-28 14:49:05 -0700

    updated default banner

--- components/banners/banner-default.php
+++ components/banners/banner-default.php
@@ -1,19 +1,12 @@
 <?php
 $post_id = isset($post_id) ? $post_id : get_the_ID();
-$title = isset($title) ? $title : get_the_title($post_id);
 
-if ($title_override = get_field('banner_title_override', $post_id)) {
-	$title = $title_override;
+if (!isset($title)) {
+	$title_override = get_field('banner_title_override', $post_id);
+	$title = trim($title_override) ? trim($title_override) : get_the_title($post_id);
 }
 
-$button_type = get_field('banner_button_type', $post_id);
-$button_text = '';
-$button_link = '';
-
-if ($button_type !== 'none') {
-	$button_text = get_field('banner_button_text', $post_id);
-	$button_link = get_field('banner_button_'.$button_type, $post_id);
-}
+$button = Blueprint\Blocks::get_button_field_values('banner_button', $post_id);
 
 $background_image_opts = ['post_id' => $post_id];
 
@@ -27,10 +20,10 @@ if (isset($background_image)) {
 	<div class="banner-default__content">
 		<h1 class="banner-default__title"><?php echo esc_html($title); ?></h1>
 		<?php
-		if ($button_text && $button_link)
+		if ($button)
 		{
 			?>
-			<a href="<?php echo esc_url($button_link); ?>" class="button banner-default__button"><?php echo esc_html($button_text); ?></a>
+			<a href="<?php echo esc_url($button->link); ?>" class="button banner-default__button"><?php echo esc_html($button->text); ?></a>
 			<?php
 		}
 		?>
