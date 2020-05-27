85761db4f5e785c886a739b216283eb05e1cae83
Doug Frei <dougfrei@gmail.com>
2020-05-27 16:48:41 -0700

    use chokidar directly in gulp build tasks to lower CPU usage when watching files

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
 const fs = require('fs');
 const path = require('path');
-const { series, parallel, src, dest, watch } = require('gulp');
-const plugins		= require('gulp-load-plugins')();
-const browsersync	= require('browser-sync').create();
-const webpack		= require('webpack');
-const webpackStream	= require('webpack-stream');
-const webpackConfig	= require('./webpack.config.js');
+const { series, parallel, src, dest } = require('gulp');
+const plugins = require('gulp-load-plugins')();
+const browsersync = require('browser-sync').create();
+const chokidar = require('chokidar');
+const webpack = require('webpack');
+const webpackStream = require('webpack-stream');
+const webpackConfig = require('./webpack.config.js');
 
 const watchPaths = {
 	scss: 'css/**/*.scss',
 	js: 'js/**/*.js',
-	php: ['**/*.php', '!vendor/**/*.php', '!node_modules/**/*.php', '!media/**/*.php', '!js/**/*.php', '!css/**/*.php', '!dist/**/*.php']
+	php: [
+		'**/*.php',
+		'!vendor/**/*.php',
+		'!node_modules/**/*.php',
+		'!media/**/*.php',
+		'!js/**/*.php',
+		'!css/**/*.php',
+		'!dist/**/*.php'
+	]
 };
 
 const srcFiles = {
@@ -20,8 +29,6 @@ const srcFiles = {
 };
 
 const distFiles = {
-	scss: 'dist/css/master.min.css',
-	scssEditorStyles: 'dist/css/editor-styles.min.css',
 	js: 'dist/js/master.min.js'
 };
 
@@ -40,29 +47,36 @@ function buildSCSSwithInput(srcFile, destPath = 'dist/css') {
 	const basename = path.basename(srcFile, '.scss');
 
 	return src(srcFile)
-		.pipe(plugins.plumber({
-			errorHandler: function (err) {
-				plugins.notify.onError({
-					title: 'SCSS Build Error',
-					message: err.message
-				})(err);
-			}
-		}))
+		.pipe(
+			plugins.plumber({
+				errorHandler: function (err) {
+					plugins.notify.onError({
+						title: 'SCSS Build Error',
+						message: err.message
+					})(err);
+				}
+			})
+		)
 		.pipe(plugins.sourcemaps.init())
 		.pipe(plugins.sass())
 		.on('error', plugins.sass.logError)
 		.pipe(plugins.autoprefixer())
-		.pipe(plugins.cleanCss({
-			keepSpecialComments: 0
-		}))
+		.pipe(
+			plugins.cleanCss({
+				keepSpecialComments: 0
+			})
+		)
 		.pipe(plugins.rename(`${basename}.min.css`))
 		.pipe(plugins.sourcemaps.write('.'))
 		.pipe(dest(destPath))
 		.pipe(browsersync.stream())
-		.on('error', plugins.notify.onError({
-			message: '<%= error.message %>',
-			title: 'SCSS Error'
-		}));
+		.on(
+			'error',
+			plugins.notify.onError({
+				message: '<%= error.message %>',
+				title: 'SCSS Error'
+			})
+		);
 }
 
 function buildSCSStheme() {
@@ -75,18 +89,22 @@ function buildSCSSeditor() {
 
 function buildJS() {
 	return src(srcFiles.js)
-		.pipe(plugins.plumber({
-			errorHandler: function (err) {
-				plugins.notify.onError({
-					title: 'JS Build Error',
-					message: err.message
-				})(err);
-			}
-		}))
-		.pipe(plugins.notify({
-			message: 'Starting JS build',
-			title: 'JS Build'
-		}))
+		.pipe(
+			plugins.plumber({
+				errorHandler: function (err) {
+					plugins.notify.onError({
+						title: 'JS Build Error',
+						message: err.message
+					})(err);
+				}
+			})
+		)
+		.pipe(
+			plugins.notify({
+				message: 'Starting JS build',
+				title: 'JS Build'
+			})
+		)
 		.pipe(webpackStream(webpackConfig, webpack))
 		.pipe(dest('dist/js'));
 }
@@ -95,12 +113,23 @@ function buildJS() {
  * Watch Tasks
  */
 function watchSourceSCSS(cb) {
-	watch(watchPaths.scss, parallel(buildSCSStheme, buildSCSSeditor));
+	chokidar.watch(watchPaths.scss).on('change', (event, filePath) => {
+		console.log('SCSS file modified', filePath);
+
+		buildSCSStheme();
+		buildSCSSeditor();
+	});
+
 	cb();
 }
 
 function watchSourceJS(cb) {
-	watch(watchPaths.js, buildJS);
+	chokidar.watch(watchPaths.js).on('change', (event, filePath) => {
+		console.log('JS file modified', filePath);
+
+		buildJS();
+	});
+
 	cb();
 }
 
@@ -111,15 +140,21 @@ function startBrowserSync(cb) {
 	const localConfig = loadLocalConfig();
 
 	if (!localConfig) {
-		cb(new Error('Unable to load "local_config.json" -- please use "local_config_example.json" as a template'));
+		cb(
+			new Error(
+				'Unable to load "local_config.json" -- please use "local_config_example.json" as a template'
+			)
+		);
 		return;
 	}
 
 	browsersync.init(localConfig.browserSync);
 
-	watch([distFiles.js].concat(watchPaths.php), series(() => {
+	chokidar.watch([distFiles.js].concat(watchPaths.php)).on('change', (event, filePath) => {
+		console.log('dist change', filePath);
+
 		browsersync.reload();
-	}));
+	});
 
 	cb();
 }
@@ -135,8 +170,17 @@ exports.build = parallel(buildSCSStheme, buildSCSSeditor, buildJS);
 
 exports.watch_scss = series(parallel(buildSCSStheme, buildSCSSeditor), watchSourceSCSS);
 exports.watch_js = series(buildJS, watchSourceJS);
-exports.watch = series(parallel(buildSCSStheme, buildSCSSeditor, buildJS), watchSourceSCSS, watchSourceJS);
+exports.watch = series(
+	parallel(buildSCSStheme, buildSCSSeditor, buildJS),
+	watchSourceSCSS,
+	watchSourceJS
+);
 
 exports.browsersync = startBrowserSync;
 
-exports.default = series(parallel(buildSCSStheme, buildSCSSeditor, buildJS), watchSourceSCSS, watchSourceJS, startBrowserSync);
+exports.default = series(
+	parallel(buildSCSStheme, buildSCSSeditor, buildJS),
+	watchSourceSCSS,
+	watchSourceJS,
+	startBrowserSync
+);
