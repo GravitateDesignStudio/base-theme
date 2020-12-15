00dd356ebca16a4992d551a0f93dde189d6cc870
Doug Frei <dougfrei@gmail.com>
2020-12-15 11:59:57 -0800

    helpers for working with foundation rows; CSS structure updates

--- components/archive/load-more.php
+++ components/archive/load-more.php
@@ -11,9 +11,9 @@ $nextpage = intval($paged) + 1;
 
 if (!is_single() && ($nextpage <= $max_page)) {
 	$button_text = $button_text ?? 'Load More';
-	
+
 	?>
-	<div class="archive__load-more-container">
+	<div class="archive__load-more-container faux-row">
 		<a href="#" class="button js__load-more"><?php echo esc_html($button_text); ?></a>
 	</div>
 	<?php
--- components/banners/banner-default.php
+++ components/banners/banner-default.php
@@ -17,7 +17,7 @@ if (isset($background_image)) {
 ?>
 <div class="banner banner-default bg-black">
 	<?php WPUtil\Component::render('components/banners/partials/banner-background', $background_image_opts); ?>
-	<div class="banner-default__content">
+	<div class="banner-default__content faux-row">
 		<h1 class="banner-default__title"><?php echo esc_html($title); ?></h1>
 		<?php
 		if ($button->link && $button->text)
