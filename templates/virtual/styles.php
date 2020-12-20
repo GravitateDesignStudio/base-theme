f7b952c4517f36dfa18980003e02e3deabdfcc3d
Doug Frei <dougfrei@gmail.com>
2020-12-19 16:32:30 -0800

    added virtual pages support

--- templates/virtual/styles.php
+++ templates/virtual/styles.php
@@ -1,10 +1,9 @@
 <?php
-/*
-*	Template Name: Styles
-*/
 get_header();
 
-WPUtil\Component::render('components/banners/banner-default');
+WPUtil\Component::render('components/banners/banner-default', [
+	'title' => 'Styles'
+]);
 
 $backgrounds = apply_filters('grav_block_background_colors', [], '');
 $backgrounds = $backgrounds ? array_filter($backgrounds, function ($key) {
@@ -23,7 +22,7 @@ if (have_posts()) {
 
 				colorSelect.addEventListener('change', function (e) {
 					var colorClass = e.currentTarget.selectedOptions[0].value;
-					
+
 					document.querySelector('#style-testing__content').setAttribute('class', colorClass);
 				});
 			});
@@ -37,21 +36,17 @@ if (have_posts()) {
 				</div>
 			</section>
 		</div>
-		<div class="style-testing__color-selector">
+		<div class="style-testing__color-selector faux-row">
 			<label for="bg-color">Background Color:</label>
 			<select id="bg-color" class="style-testing__select--color">
 				<?php
 				foreach ($backgrounds as $class => $label) {
 					?>
-					<option value="<?php echo esc_attr($class); ?>" 
-											  <?php
-												if ($class === '') {
-													?>
-						selected
-													<?php
-												}
-												?>
-					><?php echo esc_html($label); ?></option>
+					<option value="<?php echo esc_attr($class); ?>"
+						<?php if ($class === '') { ?>selected<?php } ?>
+					>
+						<?php echo esc_html($label); ?>
+					</option>
 					<?php
 				}
 				?>
@@ -59,9 +54,7 @@ if (have_posts()) {
 		</div>
 		<?php
 
-		if (class_exists('GRAV_BLOCKS')) {
-			GRAV_BLOCKS::display();
-		}
+		Blueprint\Blocks::safe_display();
 	}
 }
 
