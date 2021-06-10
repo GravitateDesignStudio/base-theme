dcfdda0ba9ea9f56864ba4ce39d32b4ced253c99
Doug Frei <dougfrei@gmail.com>
2021-06-10 10:36:42 -0700

    style template update to prevent multiple loops

--- templates/virtual/styles.php
+++ templates/virtual/styles.php
@@ -10,52 +10,43 @@ $backgrounds = $backgrounds ? array_filter($backgrounds, function ($key) {
 	return $key !== 'block-bg-image';
 }, ARRAY_FILTER_USE_KEY) : [];
 
+?>
+<script>
+	document.addEventListener('DOMContentLoaded', function (e) {
+		var colorSelect = document.querySelector('.style-testing__select--color');
 
-if (have_posts()) {
-	while (have_posts()) {
-		the_post();
+		colorSelect.addEventListener('change', function (e) {
+			var colorClass = e.currentTarget.selectedOptions[0].value;
 
-		?>
-		<script>
-			document.addEventListener('DOMContentLoaded', function (e) {
-				var colorSelect = document.querySelector('.style-testing__select--color');
-
-				colorSelect.addEventListener('change', function (e) {
-					var colorClass = e.currentTarget.selectedOptions[0].value;
-
-					document.querySelector('#style-testing__content').setAttribute('class', colorClass);
-				});
-			});
-		</script>
-		<div id="style-testing__content">
-			<section class="section-container wysiwyg">
-				<div class="row section-inner">
-					<div class="columns small-12">
-						<?php WPUtil\Component::render('components/testing/style-testing'); ?>
-					</div>
-				</div>
-			</section>
+			document.querySelector('#style-testing__content').setAttribute('class', colorClass);
+		});
+	});
+</script>
+<div id="style-testing__content">
+	<section class="section-container wysiwyg">
+		<div class="row section-inner">
+			<div class="columns small-12">
+				<?php WPUtil\Component::render('components/testing/style-testing'); ?>
+			</div>
 		</div>
-		<div class="style-testing__color-selector faux-row">
-			<label for="bg-color">Background Color:</label>
-			<select id="bg-color" class="style-testing__select--color">
-				<?php
-				foreach ($backgrounds as $class => $label) {
-					?>
-					<option value="<?php echo esc_attr($class); ?>"
-						<?php if ($class === '') { ?>selected<?php } ?>
-					>
-						<?php echo esc_html($label); ?>
-					</option>
-					<?php
-				}
-				?>
-			</select>
-		</div>
+	</section>
+</div>
+<div class="style-testing__color-selector faux-row">
+	<label for="bg-color">Background Color:</label>
+	<select id="bg-color" class="style-testing__select--color">
 		<?php
-
-		Blueprint\Blocks::safe_display();
-	}
-}
+		foreach ($backgrounds as $class => $label) {
+			?>
+			<option value="<?php echo esc_attr($class); ?>"
+				<?php if ($class === '') { ?>selected<?php } ?>
+			>
+				<?php echo esc_html($label); ?>
+			</option>
+			<?php
+		}
+		?>
+	</select>
+</div>
+<?php
 
 get_footer();
