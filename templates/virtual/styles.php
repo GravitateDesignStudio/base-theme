8ff76e9ab8de82ac85a4bb9056a363c69bd40e4b
Will Ashworth <william.ashworth@gravitatedesign.com>
2023-05-18 15:15:30 -0700

    Code cleanup for Styles

--- templates/virtual/styles.php
+++ templates/virtual/styles.php
@@ -9,8 +9,8 @@ $backgrounds = apply_filters('grav_block_background_colors', [], '');
 $backgrounds = $backgrounds ? array_filter($backgrounds, function ($key) {
 	return $key !== 'block-bg-image';
 }, ARRAY_FILTER_USE_KEY) : [];
-
 ?>
+
 <script>
 	document.addEventListener('DOMContentLoaded', function (e) {
 		var colorSelect = document.querySelector('.style-testing__select--color');
@@ -22,6 +22,7 @@ $backgrounds = $backgrounds ? array_filter($backgrounds, function ($key) {
 		});
 	});
 </script>
+
 <div id="style-testing__content">
 	<section class="section-container wysiwyg">
 		<div class="row section-inner">
@@ -31,22 +32,17 @@ $backgrounds = $backgrounds ? array_filter($backgrounds, function ($key) {
 		</div>
 	</section>
 </div>
+
 <div class="style-testing__color-selector faux-row">
 	<label for="bg-color">Background Color:</label>
 	<select id="bg-color" class="style-testing__select--color">
-		<?php
-		foreach ($backgrounds as $class => $label) {
-			?>
-			<option value="<?php echo esc_attr($class); ?>"
-				<?php if ($class === '') { ?>selected<?php } ?>
-			>
+		<?php foreach ($backgrounds as $class => $label) : ?>
+			<option value="<?php echo esc_attr($class); ?>" <?= ($class === '') ? 'selected' : ''; ?>>
 				<?php echo esc_html($label); ?>
 			</option>
-			<?php
-		}
-		?>
+		<?php endforeach; ?>
 	</select>
 </div>
-<?php
 
+<?php
 get_footer();
