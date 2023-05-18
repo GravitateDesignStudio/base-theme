2280a7411388e510d3f45a2133ab26a6be7ce155
Will Ashworth <william.ashworth@gravitatedesign.com>
2023-05-18 13:04:15 -0700

    Updated logic for banner title override filter

--- bootstrap/filters.php
+++ bootstrap/filters.php
@@ -2,9 +2,5 @@
 
 // Override the title
 add_filter('the_title', function($title, $post_id) {
-	if( $title_override = get_field('banner_title_override', $post_id) ) {
-        $title = trim($title_override) ? trim($title_override) : get_the_title($post_id);
-    }
-    
-    return $title;
+    return trim(get_field('banner_title_override', $post_id)) ?: $title;
 }, 10, 2);
