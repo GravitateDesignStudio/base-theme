98749d40f294e98a175b86540b3523bd0d31be4c
Will Ashworth <william.ashworth@gravitatedesign.com>
2023-05-18 14:47:33 -0700

    Removed filter for get_title()

--- bootstrap/filters.php
+++ bootstrap/filters.php
@@ -1,6 +1,2 @@
 <?php
-
-// Override the title
-add_filter('the_title', function($title, $post_id) {
-    return trim(get_field('banner_title_override', $post_id)) ?: $title;
-}, 10, 2);
+// silence is golden :) 
