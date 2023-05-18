c3dbe612bfff885337bfb85269b2bac2be570fd9
gravcmcguire <chris.mcguire@gravitatedesign.com>
2023-05-18 14:31:44 -0700

    home.php

--- templates/home.php
+++ templates/home.php
@@ -3,7 +3,7 @@ get_header();
 
 $blog_page_id = get_option('page_for_posts');
 
-get_template_part('components/banners/', 'banner-default', [
+get_template_part('components/banners/banner', 'default', [
 	'title' => get_the_title($blog_page_id),
 	'post_id' => $blog_page_id
 ]);
@@ -13,8 +13,8 @@ get_template_part('components/banners/', 'banner-default', [
 	<?php
 	global $wp_query;
 
-	get_template_part('components/posts-list/posts-list-blog', [
-		'wp_query_obj' => $wp_query
+	get_template_part('components/posts-list/posts-list', 'blog', [
+		'wp_query_obj' => ($wp_query && is_a($wp_query, 'WP_Query')) ? $wp_query : false,
 	]);
 	?>
 </main>
